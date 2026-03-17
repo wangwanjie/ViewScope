cd540653530e77cbf6d0c0a5be1e356ac32fc475
VanJay <vanjay.dev@gmail.com>
2026-03-18 00:56:37 +0800

    Initial commit

---
+++ ViewScope/ViewScope/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ViewScope
+//
+//  Created by VanJay on 2026/3/18.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ ViewScope/ViewScopeTests/ViewScopeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ViewScopeTests.swift
+//  ViewScopeTests
+//
+//  Created by VanJay on 2026/3/18.
+//
+
+import Testing
+@testable import ViewScope
+
+struct ViewScopeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
